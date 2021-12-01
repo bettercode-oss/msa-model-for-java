1875d6905cc8d34fc521183b046eaf947aaa2756
Dion <ksundong@gmail.com>
2021-12-02 01:37:22 +0900

    feat: 예제 코드 엔드포인트 작성

---
+++ src/main/java/kr/bettercode/msamodelforjava/controller/TransactionController.java
@@ -0,0 +1,32 @@
+package kr.bettercode.msamodelforjava.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/charge-points/{chargePointId}/transactions")
+public class TransactionController {
+
+  @ResponseStatus(HttpStatus.CREATED)
+  @PostMapping
+  public void startCharging(@PathVariable Long chargePointId) {
+    log.info("충전 시작: 충전소 id {}", chargePointId);
+  }
+
+  @PutMapping("/{transactionId}/meters")
+  public void getMeters(@PathVariable Long chargePointId, @PathVariable Long transactionId) {
+    log.info("현재 충전량 확인: 충전소 id: {}, transaction id: {}", chargePointId, transactionId);
+  }
+
+  @PutMapping("/{transactionId}/stop")
+  public void stopCharging(@PathVariable Long chargePointId, @PathVariable Long transactionId) {
+    log.info("충전 중지: 충전소 id: {}, transaction id: {}", chargePointId, transactionId);
+  }
+}
